c0d7d47544c8de17e130d507254a76a41d7aba20
harkiramadhan <harkiramadhan@gmail.com>
2018-11-22 22:35:38 +0700

    first commit

---
+++ db.sql
@@ -0,0 +1,17 @@
+nama
+jenis kelamin 
+tempat lahir 
+tanggal lahir 
+angkatan 
+status
+alamat 
+domisili 
+no. hp 
+email 
+facebook instagram twitter
+profesi / bidang yang ditekuni 
+pekerjaan saat ini 
+institusi pendidikan
+fakultas / jurusan
+riwayat organisasi 
+prestasi yang diperoleh dalam bidang yang ditekuni
